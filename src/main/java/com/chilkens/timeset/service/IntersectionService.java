cadf36c27bdb8a465f7d510027208140e46690bd
Eunong <hoodyong@gmail.com>
2017-08-05 02:24:35 +0900

    Fixed bug in getIntersection method

--- src/main/java/com/chilkens/timeset/service/IntersectionService.java
+++ src/main/java/com/chilkens/timeset/service/IntersectionService.java
@@ -22,11 +22,11 @@ public class IntersectionService {
         List<DateInfo> dateInfoList;
 
         List<PickJoin> picks = pickJoinRepository.findByTableId(tableId);
-        for(PickJoin pick : picks) {
+        for (PickJoin pick : picks) {
             dateInfoList = new ArrayList<>();
-            for(PickDetail detail : pick.getPickDetailList()) {
+            for (PickDetail detail : pick.getPickDetailList()) {
                 //time split
-                for(String time : Arrays.asList(detail.getPickTime().split("\\|"))) {
+                for (String time : Arrays.asList(detail.getPickTime().split("\\|"))) {
                     dateInfoList.add(DateInfo.build(detail.getPickDate(), Integer.valueOf(time)));
                 }
             }
@@ -46,23 +46,23 @@ public class IntersectionService {
         int i = 0;
 
         //첫번째 시간 정보 preSet에 저장
-        for(DateInfo dateInfo : picks.get(0).getDateInfos()) {
+        for (DateInfo dateInfo : picks.get(0).getDateInfos()) {
             preSet.add(dateInfo);
         }
 
-        for(PickInfo pick : picks) { //picks.subList(1, picks.size())
-            if(i++ == 0) { //첫번째정보 위에서 저장했기때문에 바로 빠져나옴
+        for (PickInfo pick : picks) { //picks.subList(1, picks.size())
+            if (i++ == 0) { //첫번째정보 위에서 저장했기때문에 바로 빠져나옴
                 continue;
             }
 
             curSet = new HashSet<>();
-            for(DateInfo dateInfo : pick.getDateInfos()) {
+            for (DateInfo dateInfo : pick.getDateInfos()) {
                 curSet.add(dateInfo);
             }
 
             preSet = Sets.intersection(preSet, curSet); // intersectionSet 구해서 preSet에 계속 저장
 
-            if(preSet == null) { //교집합이 없으면 바로 return
+            if (preSet == null) { //교집합이 없으면 바로 return
                 return null;
             }
         }
@@ -79,31 +79,32 @@ public class IntersectionService {
         List<DateInfo> intersections = getAllIntersection(tableId);
         List<DateInfo> intersectionsByTime = new ArrayList<>();
 
+        // 맨 처음에 최소시간 1이면 바로 return
+        if (minSize == 1) {
+            return intersections;
+        }
+
+        Date preDate = intersections.get(0).getDate();
         int preTime = intersections.get(0).getTime();
         int size = 0;
         int p = 0;
 
-        // 맨 처음에 최소시간 1이면 바로 return
-        if(minSize == 1) {
-            return intersections;
-        }
-
-        for(int i=1; i<intersections.size(); i++) {
-            if(preTime+1 == intersections.get(i).getTime()) {
+        for (int i = 1; i < intersections.size(); i++) {
+            if (preTime + 1 == intersections.get(i).getTime() && preDate.equals(intersections.get(i).getDate())) {
                 size++;
-            }
-            else {
-                if(size+1 >= minSize) {
-                    intersectionsByTime.addAll(intersections.subList(p, p+size+1));
+            } else {
+                if (size + 1 >= minSize) {
+                    intersectionsByTime.addAll(intersections.subList(p, p + size + 1));
                 }
                 size = 0;
                 p = i;
             }
             //last index
-            if(i == intersections.size()-1 && size+1 >= minSize) {
-                intersectionsByTime.addAll(intersections.subList(p, p+size+1));
+            if (i == intersections.size() - 1 && size + 1 >= minSize) {
+                intersectionsByTime.addAll(intersections.subList(p, p + size + 1));
             }
             preTime = intersections.get(i).getTime();
+            preDate = intersections.get(i).getDate();
         }
         return intersectionsByTime;
     }
